fc559fe121b2d807493257b07c9c627c31cda69d
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-05-19 20:39:46 -0700

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=[int(n) for n in input().split()]
+b=sorted(a)
+
+
+for i in range(0,len(b)-1,2):
+    j=i+1
+    if b[i]!=b[j]:
+        print(b[i])
+        break
+else:
+    print(b[-1])
