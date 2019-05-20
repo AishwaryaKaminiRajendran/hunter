a56351592255e4e645c2d9a0404d8467d4e379aa
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-05-19 20:56:36 -0700

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=[int(n) for n in input().split()]
+b=sorted(a)
+print(b)
+for i in range(0,len(b)-1):
+    j=i+1
+    if b[i]==b[j]:
+        print(b[i])
+        break
+else:
+    print("unique")
+    
