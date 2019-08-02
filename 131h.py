0ad981eb43c7ee4ca3983445706996d2e1fafaef
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-08-02 11:58:04 +0530

    Create 131h.py

---
+++ 131h.py
@@ -0,0 +1,16 @@
+n=int(input())
+a=[int(n) for n in input().split()]
+b=sorted(a)
+#print(b)
+c=b[::-1]
+#print(c)
+x=[]
+for i in range(0,len(c)):
+    if c[i]==len(x):
+        x.append(c[i])
+        break
+    else:
+        x.append(c[i])
+        x.append(b[i])
+print(x)
+    
